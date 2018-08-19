d1efb9933c9c4c4800ca8f6fcb8feddaff745f92
Andrew Sprague <andrewsprague@outlook.com>
2018-08-19 18:54:03 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,9 +54,9 @@ def monopoly_with_fourth_tier
 			:pieces => 4,
 			names:  {
 				:reading_railroad => {:mortgage_value => '$100'},
-				:pennsylvania_railroad => {},
-				b_and_o_railroad: {},
-				shortline: {}
+				:pennsylvania_railroad => {:mortgage_value => '$100'},
+				b_and_o_railroad: {:mortgage_value => '$100'},
+				shortline: {:mortgage_value => '$100'}
 			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
