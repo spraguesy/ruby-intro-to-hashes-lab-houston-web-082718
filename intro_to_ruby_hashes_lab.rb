fcea7310c53311814a7e503035e9100600e4c568
Andrew Sprague <andrewsprague@outlook.com>
2018-08-19 18:55:56 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 		:railroads => {
 			:pieces => 4,
 			names:  {
-				:reading_railroad => {:"mortgage_value" => '$100'},
+				:reading_railroad => {"mortgage_value" => '$100'},
 				:pennsylvania_railroad => {:mortgage_value => '$100'},
 				b_and_o_railroad: {:mortgage_value => '$100'},
 				shortline: {:mortgage_value => '$100'}
